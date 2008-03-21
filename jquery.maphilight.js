50fa120e9e9288f13fc0abc2b8f3ae6415bf70e7
David Lynch <david@chapel.(none)>
2008-03-20 21:01:32 -0700

    More syntax tweaks to satisfy the YUI compressor on --verbose.

--- jquery.maphilight.js
+++ jquery.maphilight.js
@@ -1,6 +1,8 @@
 (function($) {
-	var has_VML = document.namespaces;
-	var has_canvas = document.createElement('canvas');
+	var has_VML, create_canvas_for, add_shape_to, clear_canvas, shape_from_area,
+		canvas_style, fader, hex_to_decimal, css3color;
+	has_VML = document.namespaces;
+	has_canvas = document.createElement('canvas');
 	has_canvas = has_canvas && has_canvas.getContext;
 	
 	if(!(has_canvas || has_VML)) {
@@ -8,19 +10,18 @@
 		return;
 	}
 	
-	var create_canvas_for, add_shape_to, clear_canvas;
 	if(has_canvas) {
-		var fader = function(element, opacity, interval) {
+		fader = function(element, opacity, interval) {
 			if(opacity <= 1) {
 				element.style.opacity = opacity;
 				window.setTimeout(fader, 10, element, opacity + 0.1, 10);
 			}
 		};
 		
-		var hex_to_decimal = function(hex) {
+		hex_to_decimal = function(hex) {
 			return Math.max(0, Math.min(parseInt(hex, 16), 255));
 		};
-		var css3color = function(color, opacity) {
+		css3color = function(color, opacity) {
 			return 'rgba('+hex_to_decimal(color.substr(0,2))+','+hex_to_decimal(color.substr(2,2))+','+hex_to_decimal(color.substr(4,2))+','+opacity+')';
 		};
 		create_canvas_for = function(img) {
@@ -29,13 +30,13 @@
 			return c;
 		};
 		add_shape_to = function(canvas, shape, coords, options) {
-			var context = canvas.getContext('2d');
+			var i, context = canvas.getContext('2d');
 			context.beginPath();
 			if(shape == 'rect') {
 				context.rect(coords[0], coords[1], coords[2] - coords[0], coords[3] - coords[1]);
 			} else if(shape == 'poly') {
 				context.moveTo(coords[0], coords[1]);
-				for(var i=2; i < coords.length; i+=2) {
+				for(i=2; i < coords.length; i+=2) {
 					context.lineTo(coords[i], coords[i+1]);
 				}
 			} else if(shape == 'circ') {
@@ -59,22 +60,21 @@
 			canvas.getContext('2d').clearRect(0, 0, canvas.width,canvas.height);
 		};
 	} else {
-		var stylesheet = document.createStyleSheet();
-		stylesheet.addRule("v\\:*", "behavior: url(#default#VML); antialias: true;"); 
+		document.createStyleSheet().addRule("v\\:*", "behavior: url(#default#VML); antialias: true;"); 
 		document.namespaces.add("v", "urn:schemas-microsoft-com:vml"); 
 		
 		create_canvas_for = function(img) {
 			return $('<var style="zoom:1;overflow:hidden;display:block;width:'+img.width+'px;height:'+img.height+'px;"></var>').get(0);
 		};
 		add_shape_to = function(canvas, shape, coords, options) {
-			var fill = '<v:fill color="#'+options.fillColor+'" opacity="'+(options.fill ? options.fillOpacity : 0)+'" />';
-			var stroke = (options.stroke ? 'strokeweight="'+options.strokeWidth+'" stroked="t" strokecolor="#'+options.strokeColor+'"' : 'stroked="f"');
-			var opacity = '<v:stroke opacity="'+options.strokeOpacity+'"/>';
+			var fill, stroke, opacity;
+			fill = '<v:fill color="#'+options.fillColor+'" opacity="'+(options.fill ? options.fillOpacity : 0)+'" />';
+			stroke = (options.stroke ? 'strokeweight="'+options.strokeWidth+'" stroked="t" strokecolor="#'+options.strokeColor+'"' : 'stroked="f"');
+			opacity = '<v:stroke opacity="'+options.strokeOpacity+'"/>';
 			if(shape == 'rect') {
 				canvas.innerHTML = '<v:rect filled="t" '+stroke+' style="zoom:1;margin:0;padding:0;display:block;position:absolute;left:'+coords[0]+'px;top:'+coords[1]+'px;width:'+(coords[2] - coords[0])+'px;height:'+(coords[3] - coords[1])+'px;">'+fill+opacity+'</v:rect>';
 			} else if(shape == 'poly') {
-				var path = coords.join(',');
-				canvas.innerHTML = '<v:shape filled="t" '+stroke+' coordorigin="0,0" coordsize="'+canvas.width+','+canvas.height+'" path="m '+coords[0]+','+coords[1]+' l '+path+' x e" style="zoom:1;margin:0;padding:0;display:block;position:absolute;top:0px;left:0px;width:'+canvas.width+'px;height:'+canvas.height+'px;">'+fill+opacity+'</v:shape>'; 
+				canvas.innerHTML = '<v:shape filled="t" '+stroke+' coordorigin="0,0" coordsize="'+canvas.width+','+canvas.height+'" path="m '+coords[0]+','+coords[1]+' l '+coords.join(',')+' x e" style="zoom:1;margin:0;padding:0;display:block;position:absolute;top:0px;left:0px;width:'+canvas.width+'px;height:'+canvas.height+'px;">'+fill+opacity+'</v:shape>'; 
 			} else if(shape == 'circ') {
 				canvas.innerHTML = '<v:oval filled="t" '+stroke+' style="zoom:1;margin:0;padding:0;display:block;position:absolute;left:'+(coords[0] - coords[2])+'px;top:'+(coords[1] - coords[2])+'px;width:'+(coords[2]*2)+'px;height:'+(coords[2]*2)+'px;">'+fill+opacity+'</v:oval>';
 			}
@@ -83,13 +83,13 @@
 			canvas.innerHTML = '';
 		};
 	}
-	var shape_from_area = function(area) {
-		var coords = area.getAttribute('coords').split(',');
-		for (var i=0; i < coords.length; i++) { coords[i] = parseFloat(coords[i]); }
+	shape_from_area = function(area) {
+		var i, coords = area.getAttribute('coords').split(',');
+		for (i=0; i < coords.length; i++) { coords[i] = parseFloat(coords[i]); }
 		return [area.getAttribute('shape').toLowerCase().substr(0,4), coords];
 	};
 	
-	var canvas_style = {
+	canvas_style = {
 		position: 'absolute',
 		left: 0,
 		top: 0,
@@ -99,26 +99,27 @@
 	$.fn.maphilight = function(opts) {
 		opts = $.extend({}, $.fn.maphilight.defaults, opts);
 		return this.each(function() {
-			var img = $(this);
-			var options = $.metadata ? $.extend({}, opts, img.metadata()) : opts;
-			var map = $('map[name="'+img.attr('usemap').substr(1)+'"]');
+			var img, options, map, canvas, mouseover;
+			img = $(this);
+			options = $.metadata ? $.extend({}, opts, img.metadata()) : opts;
+			map = $('map[name="'+img.attr('usemap').substr(1)+'"]');
 			if(!(img.is('img') && img.attr('usemap') && map.size() > 0 && !img.hasClass('maphilighted'))) { return; }
 			img.wrap($('<div style="background:url('+this.src+');position: relative; padding: 0; width: '+this.width+'px; height: '+this.height+'px"></div>'));
 			img.css('opacity', 0).css('border', 0).css(canvas_style);
 			if($.browser.msie) { img.css('filter', 'Alpha(opacity=0)'); }
 			
-			var canvas = create_canvas_for(this);
+			canvas = create_canvas_for(this);
 			$(canvas).css(canvas_style);
 			canvas.height = this.height;
 			canvas.width = this.width;
 			
-			var mouseover = function(e) {
+			mouseover = function(e) {
 				var shape = shape_from_area(this);
 				add_shape_to(canvas, shape[0], shape[1], $.metadata ? $.extend({}, options, $(this).metadata()) : options);
 			};
 			
 			if(options.alwaysOn) {
-				var areas = $(map).find('area[coords]').each(mouseover);
+				$(map).find('area[coords]').each(mouseover);
 			} else {
 				$(map).find('area[coords]').mouseover(mouseover).mouseout(function(e) { clear_canvas(canvas); });
 			}
