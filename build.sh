5dd1b6b60a4eaeab04d980a8fbd7e48617878f6b
Zach Dennis <zach.dennis@gmail.com>
2009-12-22 09:31:34 -0500

    updated build.sh to refer to a generic yuicompressor build rather than a specific version

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar ../yuicompressor-2.3.5/build/yuicompressor-2.3.5.jar -o jquery.maphilight.min.js jquery.maphilight.js
+java -jar ../yuicompressor/build/*.jar -o jquery.maphilight.min.js jquery.maphilight.js
