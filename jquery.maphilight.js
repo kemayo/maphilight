3ab6f8e35a5988a48384ffc4642106b314f0feca
Zach Tirrell <zbtirrell@gmail.com>
2014-04-02 07:49:33 -0400

    adding an explicit background size for the wrapper div
    This will allow the map hilights to scale appropriately.  This is especially helpful if you want to use a hidpi version of your map (pixel doubled)
    
    I know that a similar approach was included in this open pull request: https://github.com/kemayo/maphilight/pull/36
    
    However, I think this one is more generally compatible and *only* contains the background sizing modification.

--- jquery.maphilight.js
+++ jquery.maphilight.js
@@ -245,6 +245,7 @@
 			wrap = $('<div></div>').css({
 				display:'block',
 				background:'url("'+this.src+'")',
+				backgroundSize: this.width + 'px ' + this.height + 'px',
 				position:'relative',
 				padding:0,
 				width:this.width,
